bf3cf50eabdfe1c82827f1b1162d4ae09ddb7331
Charles Painter <cpainter85@gmail.com>
2015-04-18 20:36:24 -0600

    ch 3 exercise recursion

---
+++ ch3-exercises/recursion.js
@@ -0,0 +1,14 @@
+var isEven = function(number) {
+  if (number === 0) {
+    return true;
+  }
+  else if (number === 1) {
+    return false;
+  }
+  else if (number < 0) {
+    return isEven(number + 2);
+  }
+  else {
+    return isEven(number - 2);
+  }
+};
