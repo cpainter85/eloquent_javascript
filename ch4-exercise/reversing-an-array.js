14b71aab6ccb83adffbc586bc55f8e8312460d9b
Charles Painter <cpainter85@gmail.com>
2015-04-18 22:49:01 -0600

    reverseArrayInPlace function

--- ch4-exercise/reversing-an-array.js
+++ ch4-exercise/reversing-an-array.js
@@ -5,3 +5,12 @@ var reverseArray = function (array) {
   }
   return result;
 }
+
+var reverseArrayInPlace = function(array) {
+  for (var index = 0; index < Math.floor(array.length/2); index ++){
+    var value_holder = array[index];
+    array[index] = array[array.length - 1 - index];
+    array[array.length - 1 - index] = value_holder;
+  }
+  return array;
+}
