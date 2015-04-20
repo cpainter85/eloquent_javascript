8a664de4baeecf86dc7013914570edbe40c039a5
Charles Painter <cpainter85@gmail.com>
2015-04-20 16:10:46 -0600

    finish a list exercise

--- ch4-exercise/a-list.js
+++ ch4-exercise/a-list.js
@@ -5,3 +5,27 @@ var arrayToList = function (array) {
   }
   return list;
 };
+
+var listToArray = function (list) {
+  var array = [];
+  for (var listSection = list; listSection; listSection = listSection.rest){
+    array.push(listSection.value);
+  }
+  return array;
+};
+
+var prepend = function(element, list) {
+  return {value: element, rest: list};
+};
+
+var nth = function(list, position) {
+  for (var index = 0; index < position; index++) {
+    if (list.rest !== null){
+    	list = list.rest;
+    }
+    else {
+      return undefined;
+    }
+  }
+  return list.value;
+};
