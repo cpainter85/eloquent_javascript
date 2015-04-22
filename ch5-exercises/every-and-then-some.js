1e5462e9d89966153c003d4faf2b4c67c9beaf66
Charles Painter <cpainter85@gmail.com>
2015-04-22 16:26:13 -0600

    ch 5 some function

--- ch5-exercises/every-and-then-some.js
+++ ch5-exercises/every-and-then-some.js
@@ -6,3 +6,12 @@ var every = function (array, predicate_function) {
   }
   return true;
 };
+
+var some = function (array, predicate_function) {
+  for (var i =0; i < array.length; i++ ) {
+    if (predicate_function(array[i])) {
+      return true;
+    }
+  }
+  return false;
+};
