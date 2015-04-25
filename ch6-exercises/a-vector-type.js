21895a3328ae365555539ad33652f4c88f31fbf4
Charles Painter <cpainter85@gmail.com>
2015-04-25 16:07:14 -0600

    working constructor and plus prototype for vector

---
+++ ch6-exercises/a-vector-type.js
@@ -0,0 +1,21 @@
+// Your code here.
+
+function Vector(x,y) {
+  this.x = x;
+  this.y = y;
+}
+
+Vector.prototype.plus = function(secondVector) {
+  this.x += secondVector.x;
+  this.y += secondVector.y;
+  return this;
+};
+//var test = new Vector(1,3);
+//console.log(test.x);
+//console.log(test.y);
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+//console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+//console.log(new Vector(3, 4).length);
+// → 5
